f034aaa0626f11657f8dd208e29ff30568cd6065
Ayushi <ayushi.jha20052006@gmail.com>
2026-05-28 20:27:43 +0530

    Vercel Deployment

--- Backend/main.py
+++ Backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://data-lens-sepia.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
